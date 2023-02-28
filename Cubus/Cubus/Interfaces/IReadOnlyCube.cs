f4fe05f0be4b0263db49001c7c6ebfd3de695e80
Jürgen Hock <juergen.hock@jurihock.de>
2023-02-28 21:16:10 +0100

    Update docs

--- Cubus/Cubus/Interfaces/IReadOnlyCube.cs
+++ Cubus/Cubus/Interfaces/IReadOnlyCube.cs
@@ -2,8 +2,12 @@ using System;
 
 namespace Cubus
 {
+  /// <summary>
+  /// Indicates a read only cube,
+  /// which may throw an exception
+  /// on the write access.
+  /// </summary>
   public interface IReadOnlyCube
   {
   }
 }
-
